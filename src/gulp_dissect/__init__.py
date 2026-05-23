782fe8041d83eca45048a4357d8d137a47b761c9
valerino <xoanino@gmail.com>
2026-05-23 20:20:20 +0000

    forgot commit :)

--- src/gulp_dissect/__init__.py
+++ src/gulp_dissect/__init__.py
@@ -3,6 +3,6 @@
 __all__ = ["__version__"]
 
 try:
-	from ._version import version as __version__
+    from ._version import version as __version__
 except ImportError:
-	__version__ = "0.0.0"
+    __version__ = "0.0.0"
